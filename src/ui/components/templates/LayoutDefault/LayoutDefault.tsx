8a21ffd4e5d6bea0a37c116ab0a57c5921e6ce87
Oleksandr <alexracovets@gmail.com>
2025-09-25 09:03:48 +0300

    fix build

--- src/ui/components/templates/LayoutDefault/LayoutDefault.tsx
+++ src/ui/components/templates/LayoutDefault/LayoutDefault.tsx
@@ -1,4 +1,10 @@
-import { calibri, stalker, roboto, roboto_condensed, rethink_sans } from "@fonts";
+import {
+  calibri,
+  stalker,
+  roboto,
+  roboto_condensed,
+  rethink_sans,
+} from "@fonts";
 import { cn } from "@utils";
 
 import "@styles/globals.scss";
@@ -11,7 +17,7 @@ export const LayoutDefault = ({ children }: React.PropsWithChildren) => {
           calibri.variable,
           stalker.variable,
           roboto_condensed.variable,
-          roboto.variable, 
+          roboto.variable,
           rethink_sans.variable,
           "antialiased bg-main-black text-main-white relative w-[100dvw] h-[100dvh] max-w-[100%] max-h-[100%] overflow-hidden"
         )}
