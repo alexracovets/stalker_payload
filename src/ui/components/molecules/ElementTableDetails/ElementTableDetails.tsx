7d6048915241832185564e8b14a6c62a56c37b5e
Oleksandr <alexracovets@gmail.com>
2025-12-08 22:05:05 +0200

    remove scroll

--- src/ui/components/molecules/ElementTableDetails/ElementTableDetails.tsx
+++ src/ui/components/molecules/ElementTableDetails/ElementTableDetails.tsx
@@ -5,36 +5,36 @@ import { ElementsPage, Media } from "@payload-types";
 import { AtomImage, AtomWrapper, AtomText } from "@atoms";
 
 interface ElementTableDetailsProps {
-    items: ElementsPage["detaile_table_wrapper"];
+  items: NonNullable<ElementsPage["armor_group"]>["detaile_table_wrapper"];
 }
 
 export const ElementTableDetails = ({ items }: ElementTableDetailsProps) => {
-    console.log("items", items);
-    return (
-        <AtomWrapper variant="details_list" asChild>
-            <ul>
-                {
-                    items?.map((item, idx) => (
-                        <AtomWrapper variant="details_item" key={idx} asChild>
-                            <li>
-                                {typeof item?.indicator === 'object' && item.indicator !== null && (
-                                    <AtomWrapper variant="details_item_indicator">
-                                        <AtomImage image={item.indicator.image as Media} variant="table_icon" />
-                                        <AtomText variant="table_title">
-                                            {item.indicator.title}
-                                        </AtomText>
-                                    </AtomWrapper>
-                                )}
-                                <AtomWrapper variant="details_item_value">
-                                    <AtomText variant="table_title">
-                                        {item.value}
-                                    </AtomText>
-                                </AtomWrapper>
-                            </li>
-                        </AtomWrapper>
-                    ))
-                }
-            </ul>
-        </AtomWrapper>
-    );
+  console.log("items", items);
+  return (
+    <AtomWrapper variant="details_list" asChild>
+      <ul>
+        {items?.map((item, idx) => (
+          <AtomWrapper variant="details_item" key={idx} asChild>
+            <li>
+              {typeof item?.indicator === "object" &&
+                item.indicator !== null && (
+                  <AtomWrapper variant="details_item_indicator">
+                    <AtomImage
+                      image={item.indicator.image as Media}
+                      variant="table_icon"
+                    />
+                    <AtomText variant="table_title">
+                      {item.indicator.title}
+                    </AtomText>
+                  </AtomWrapper>
+                )}
+              <AtomWrapper variant="details_item_value">
+                <AtomText variant="table_title">{item.value}</AtomText>
+              </AtomWrapper>
+            </li>
+          </AtomWrapper>
+        ))}
+      </ul>
+    </AtomWrapper>
+  );
 };
--- src/ui/components/templates/TemplateSuit/TemplateSuit.tsx
+++ src/ui/components/templates/TemplateSuit/TemplateSuit.tsx
@@ -2,13 +2,7 @@
 
 import { ElementsPage, Media } from "@payload-types";
 
-import {
-  AtomHR,
-  AtomText,
-  AtomWrapper,
-  RichText,
-  AtomImage,
-} from "@atoms";
+import { AtomHR, AtomText, AtomWrapper, RichText, AtomImage } from "@atoms";
 import { ElementTableResistance, ElementTableDetails } from "@molecules";
 
 interface TemplateSuitProps {
@@ -17,34 +11,33 @@ interface TemplateSuitProps {
 
 export const TemplateSuit = ({ data }: TemplateSuitProps) => {
   const suitData = data.armor_group;
-
   return (
-      <AtomWrapper variant="content_element">
-        <AtomWrapper variant="content_header">
-          <AtomWrapper variant="content_top_wrapper">
-            <AtomWrapper variant="content_top_title">
-              <AtomText variant="h1" asChild>
-                <h1>{data.title}</h1>
-              </AtomText>
-              <AtomText variant="description">{data.sub_title}</AtomText>
-            </AtomWrapper>
-          </AtomWrapper>
-        </AtomWrapper>
-        <AtomWrapper variant="content_suits_wrapper">
-          <AtomWrapper variant="content_suits_wrapper_left">
-            <AtomHR variant="default" />
-            <RichText text={data.description} />
-            {suitData && (
-              <ElementTableResistance items={suitData.armor_table_wrapper} />
-            )}
-          </AtomWrapper>
-          <AtomWrapper variant="content_suits_wrapper_right">
-            <AtomImage image={data.image as Media} variant="element_suit" />
-            {suitData && (
-              <ElementTableDetails items={suitData.detaile_table_wrapper} />
-            )}
+    <AtomWrapper variant="content_element">
+      <AtomWrapper variant="content_header">
+        <AtomWrapper variant="content_top_wrapper">
+          <AtomWrapper variant="content_top_title">
+            <AtomText variant="h1" asChild>
+              <h1>{data.title}</h1>
+            </AtomText>
+            <AtomText variant="description">{data.sub_title}</AtomText>
           </AtomWrapper>
         </AtomWrapper>
       </AtomWrapper>
+      <AtomWrapper variant="content_suits_wrapper">
+        <AtomWrapper variant="content_suits_wrapper_left">
+          <AtomHR variant="default" />
+          <RichText text={data.description} />
+          {suitData && (
+            <ElementTableResistance items={suitData.armor_table_wrapper} />
+          )}
+        </AtomWrapper>
+        <AtomWrapper variant="content_suits_wrapper_right">
+          <AtomImage image={data.image as Media} variant="element_suit" />
+          {suitData && (
+            <ElementTableDetails items={suitData.detaile_table_wrapper } />
+          )}
+        </AtomWrapper>
+      </AtomWrapper>
+    </AtomWrapper>
   );
 };
